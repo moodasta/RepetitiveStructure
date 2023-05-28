6824d93d2abe98fefd8c294534fa8adbbccd2183
Gabrielle Oliveira <gabrielleoliveir4@gmail.com>
2023-05-28 17:03:57 -0300

    Add files via upload
    program that repeats reading a password until it is valid

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VerificacaoSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senhaCorreta = 2002;
+
+            while (true)
+            {
+                Console.Write("Digite a senha: ");
+                //lendo a entrada do usuário que retorna uma string contendo o valor digitado pelo usuário
+                //converte essa string em um valor inteiro.
+                int senha = int.Parse(Console.ReadLine());
+
+                if (senha == senhaCorreta)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha Inválida");
+                }
+            }
+        }
+    }
+}
+
